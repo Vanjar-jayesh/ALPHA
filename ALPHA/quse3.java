5b948437bba50e060feadb45692d1ecc4f27c550
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-29 10:54:14 +0530

    Add files via upload

---
+++ ALPHA/quse3.java
@@ -0,0 +1,12 @@
+public class quse3 {
+    public static void main(String[] args) {
+        int n = 100;
+
+        for(int i=10; i <=n; i++ ){
+            if (i%2==0) {
+                System.out.println(i);
+                
+            }
+        }
+    }
+}
