8f1dd9277b74d3b7db9aff73c8ad9a5be006456f
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-29 10:53:46 +0530

    Add files via upload

---
+++ ALPHA/Quse1.java
@@ -0,0 +1,11 @@
+public class Quse1 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        double are;
+
+        are = a * a * Math.PI;
+
+        System.out.println(are);
+    }
+}
