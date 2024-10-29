a6ee13ac6c1955bbaf7e46dae334a2c7cd3b5529
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-29 10:54:00 +0530

    Add files via upload

---
+++ ALPHA/Quse2.java
@@ -0,0 +1,15 @@
+public class Quse2 {
+    public static void main(String[] args) {
+        int a =10;
+        int b  =4;                                                                                                              
+
+        if (a >b) {
+            System.out.println("a is greatest then b :");
+            
+
+        }
+        else{
+            System.out.println("b is small then a :");
+        }
+    }
+}
