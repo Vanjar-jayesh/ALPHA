06b683bafab7ee17f224b826d2f40c4a444fdf4b
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-29 10:54:31 +0530

    Add files via upload

---
+++ ALPHA/quse4.java
@@ -0,0 +1,24 @@
+public class quse4 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int c = 0;
+        int s = 25;
+
+        sum = sum + s;
+
+        c = c + 1;
+        double ave;
+        // while (c == s) {
+        //         ave = sum / s;
+        //         System.out.println(ave);
+        // }
+       // System.out.println(ave);
+
+       if (c == 25) {
+        ave = sum/25;
+        System.out.println(ave);
+       }
+     
+    }
+
+}
