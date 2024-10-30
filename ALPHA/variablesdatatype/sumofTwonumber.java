dc424a952398c299b9783dea3adb51e7f6da2199
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-30 22:21:46 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/sumofTwonumber.java
@@ -0,0 +1,10 @@
+package variablesdatatype;
+
+public class sumofTwonumber {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
