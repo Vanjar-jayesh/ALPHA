ae7f0b7ed854144231948d11ad45ca81418051f4
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-30 22:22:33 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/typepromotion.java
@@ -0,0 +1,22 @@
+package variablesdatatype;
+
+public class typepromotion {
+    public static void main(String[] args) {
+        // char a = 'a'; 
+
+        // char b = 'b';
+        // System.out.println((int)(b));
+        // System.out.println((int)(a));
+        // System.out.println(b-a);
+
+
+        // byte b = 5;
+        // b = b*2;
+
+        //right
+        byte b = 5;
+        b = (byte)(b*2);
+        System.out.println(b);
+
+    }
+}
