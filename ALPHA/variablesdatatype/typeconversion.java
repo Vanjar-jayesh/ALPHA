cea52957b85106654054ed0c6ca269be11d03e6d
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-30 22:22:18 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/typeconversion.java
@@ -0,0 +1,11 @@
+package variablesdatatype;
+
+public class typeconversion {
+    public static void main(String[] args) {
+       // int a  = 25;  
+        long a = 25;
+        // int b = a;
+        System.out.println(a);
+
+    }
+}
