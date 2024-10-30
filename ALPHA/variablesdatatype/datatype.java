48563d5682f5c6701ad9f583efed654eae504aa4
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-30 21:16:44 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/datatype.java
@@ -0,0 +1,19 @@
+package variablesdatatype;
+
+public class datatype {
+    public static void main(String[] args) {
+        byte b = 8; 
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        float price = 10.5f;
+        int number = 25;
+
+        short n = 240;
+
+        System.out.println(var+"\n"+price+"\n"+number+"\n"+n);
+
+    }
+}
