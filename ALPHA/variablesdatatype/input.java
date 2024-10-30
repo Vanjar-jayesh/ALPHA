32a896cabe4894d85bd4f9b864a27e0484906aa3
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-30 22:21:10 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/input.java
@@ -0,0 +1,15 @@
+package variablesdatatype;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short input = sc.nextShort(); 
+
+
+
+        System.out.println(input);
+
+    }
+}
