838495abdb474529e8222cc5e179cc74c8d79318
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-30 22:22:01 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/typeCasing.java
@@ -0,0 +1,18 @@
+package variablesdatatype;
+
+public class typeCasing {
+    public static void main(String[] args) {
+
+        // float a = 25.12f;
+
+        // int b = (int) a;
+
+        // System.out.println(b);
+
+        char ch = 'a';
+
+        int number = ch;
+        System.out.println(number);
+    }
+}
+ 
