7cf8ada25d23303b321384f25ce1887c87a47635
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-31 14:13:42 +0530

    Update Quse3.java

--- ALPHA/variablesdatatype/Quse3.java
+++ ALPHA/variablesdatatype/Quse3.java
@@ -1,3 +1,5 @@
+// Entercostof3itemsfromtheuser(usingfloatdatatype)-apencil,apenandan eraser. You have to output the total cost of the items back to the user as their bill.(Add on : You can also try adding 18% gst tax to the items in the bill as an advanced problem)
+
 package variablesdatatype;
 import java.util.*;
 
