0d2468231390cb75d64c823c6fac36980153e8b6
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-31 14:10:24 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/Ques1.java
@@ -0,0 +1,14 @@
+package variablesdatatype;
+public class Ques1 {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 20;
+        int c = 20;
+
+        int sum = a+b+c;
+
+        double N = sum/3;
+
+        System.out.println(N);
+    }
+}
