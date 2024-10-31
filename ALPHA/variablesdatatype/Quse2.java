526ab1616f570aabf3e2cf95a31fe6c9ae9bbd22
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-31 14:10:42 +0530

    Add files via upload

---
+++ ALPHA/variablesdatatype/Quse2.java
@@ -0,0 +1,9 @@
+package variablesdatatype;
+public class Quse2 {
+    public static void main(String[] args) {
+        int a= 10;
+        
+        int square = a*a;
+        System.out.println(square);
+    }
+}
