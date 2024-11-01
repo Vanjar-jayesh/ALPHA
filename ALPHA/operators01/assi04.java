7ba95d971e8af4b5286cee7e045bfed8dc498f8c
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-01 21:05:58 +0530

    Add files via upload

---
+++ ALPHA/operators01/assi04.java
@@ -0,0 +1,14 @@
+public class assi04 {
+    public static void main(String[] args) {
+        int a =10;
+        int b =5;
+
+        a += 10;
+
+        // b = b-5;
+        b -=5;
+        System.out.println(b);
+
+        System.out.println(a);
+    }
+}
