4e8b498431d7a3c689dc8c771276ec8afb3f7670
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-01 21:07:08 +0530

    Add files via upload

---
+++ ALPHA/operators01/a01.java
@@ -0,0 +1,14 @@
+
+public class a01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a--;
+        
+        System.out.println(a);
+        System.out.println(b);
+
+        
+        System.out.println("add= "+(a+b));
+
+    }
+}
