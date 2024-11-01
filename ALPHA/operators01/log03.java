3912c65ba1dddeea2ab822d038829d62c4649b12
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-01 21:07:23 +0530

    Add files via upload

---
+++ ALPHA/operators01/log03.java
@@ -0,0 +1,11 @@
+public class log03 {
+    public static void main(String[] args) {
+        // logical ANd
+        System.out.println((3>2) && (5>0));
+        // Logical OR
+        System.out.println((3<2)||(5>0));
+
+        // Logical NOT
+        System.out.println(!(0>5));
+   }
+}
