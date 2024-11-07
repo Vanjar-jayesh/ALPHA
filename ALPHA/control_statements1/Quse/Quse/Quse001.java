05367d96765648bd4f7d801b64f2c7592e52f414
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-07 13:57:18 +0530

    Add files via upload

---
+++ ALPHA/control_statements1/Quse/Quse/Quse001.java
@@ -0,0 +1,9 @@
+package Quse;
+
+public class Quse001 {
+    public static void main(String[] args) {
+        for (int i = 0; i <=1; i++) {
+            System.out.println("Hello ");
+        }
+    }
+}
