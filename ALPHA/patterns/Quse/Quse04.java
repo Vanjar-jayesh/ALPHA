5a0368181264022b2845cf3e0563a981bc1ff486
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-09 19:46:19 +0530

    Add files via upload

---
+++ ALPHA/patterns/Quse/Quse04.java
@@ -0,0 +1,16 @@
+package Quse;
+
+public class Quse04 {
+    public static void main(String[] args) {
+        int n=4;
+        char ch ='A';
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
