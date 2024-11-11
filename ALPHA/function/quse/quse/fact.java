9f57d3b295fb3b0851a05dbe862f3893046400a9
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-11 21:09:15 +0530

    Add files via upload

---
+++ ALPHA/function/quse/quse/fact.java
@@ -0,0 +1,32 @@
+package quse;
+
+public class fact {
+    public static int fact(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+        return f;
+    }
+
+    public static int binomialcoefficient(int n,int r){
+        int nfact = fact(n);
+        int rfact = fact(r);
+        int nrfact = fact(n-r);
+
+        System.out.println(nfact);
+        System.out.println(rfact);
+        System.out.println(nrfact);
+
+        int bc = nfact/(rfact*nrfact);
+        return bc;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        int r = 2;
+
+        int bc = binomialcoefficient(n, r);
+        System.out.println(bc);
+
+    }
+}
---
+++ ALPHA/function/quse/quse/fun01.java
@@ -0,0 +1,12 @@
+package quse;
+
+public class fun01 {
+
+    public static void jay(){
+        System.out.println("helloword");
+    }
+    public static void main(String[] args) {
+        
+        jay();
+    }
+}
