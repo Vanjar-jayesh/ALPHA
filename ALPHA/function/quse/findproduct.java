dc6d9a0e75f361bd0a3943e00029cd53272691f5
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-11 21:07:55 +0530

    Add files via upload

---
+++ ALPHA/function/quse/findproduct.java
@@ -0,0 +1,14 @@
+package quse;
+
+public class findproduct {
+    public static int product(int a,int b){
+        int prod = a*b;
+        return prod;
+    }
+    public static void main(String[] args) {
+        int a = 3;
+        int b=5;
+        int  ans = product(a, b);
+        System.out.println(ans);
+    }
+}
