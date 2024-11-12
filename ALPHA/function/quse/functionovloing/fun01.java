f1bb9778888ec910095d8569e0a9ee41ea09d012
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-12 12:02:21 +0530

    Add files via upload

---
+++ ALPHA/function/quse/functionovloing/fun01.java
@@ -0,0 +1,13 @@
+package functionovloing;
+public class fun01 {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static float sum(float a,float b){
+     return a+b;   
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(3, 5));
+        System.out.println(sum(3.2f, 4.8f));
+    }
+}
