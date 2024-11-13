8cdf5da949ac171e05283c5c7a904703a4d555a1
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-13 20:20:19 +0530

    Add files via upload

---
+++ ALPHA/patterns/Quse/Quse09.java
@@ -0,0 +1,22 @@
+package Quse;
+public class Quse09 {
+
+    public static void Hollow_rectangle(int totrows,int totcols){
+
+        for(int i =1; i<=totrows; i++){
+            for(int j=1; j<=totcols; j++){
+                // cell - (i,j);
+                if (i==1||i==totrows||j==1 || j==totcols) {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Hollow_rectangle(7,10);
+    }
+}
