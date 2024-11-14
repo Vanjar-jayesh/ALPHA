91b9cd20c0a9a6970744b74ca61c5d7f9bc5dd4e
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-14 17:35:00 +0530

    Add files via upload

---
+++ ALPHA/patterns/Quse/Quse11.java
@@ -0,0 +1,15 @@
+package Quse;
+
+public class Quse11 {
+    public static void inverted_half_pyramid(int n){
+            for(int i= 1; i<=n; i++){
+                for(int j=1; j<=n-i+1;j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+    }
+    public static void main(String[] args) {
+        inverted_half_pyramid(5);
+    }
+}
