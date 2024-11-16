03602d981b9f9cd698e1dbe40615ba5b87405fcc
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-16 08:00:03 +0530

    Add files via upload

---
+++ Arrays/input_outpu.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class input_outpu {
+    public static void main(String[] args) {
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("phy:" + marks[0]);
+        System.out.println("Chem:" + marks[1]);
+        System.out.println("math:" + marks[2]);
+
+        // marks[2]=100;
+        // System.out.println("math:"+marks[0]);
+
+        int per = (marks[0]+marks[1]+marks[2])/3;
+        System.out.println("per="+per+"%");
+    }
+}
