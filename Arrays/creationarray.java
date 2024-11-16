d44de1a9545ce4fadb9a3b61bdf892bc7fffc9ab
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-16 07:38:26 +0530

    Add files via upload

---
+++ Arrays/creationarray.java
@@ -0,0 +1,16 @@
+public class creationarray {
+
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+
+        int numbers[] ={1,2,3};
+
+        int moreNumers[] = {4,5,6};
+
+        String fruits[] = {"apple","mange","orange"};
+
+        for(int i=0;i<=fruits.length; i++){
+            System.out.println(fruits[i]);
+        }
+    }
+}
