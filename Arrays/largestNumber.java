511712c043d83dfedc1701616381a9b74fccbee7
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-24 12:16:47 +0530

    Add files via upload

---
+++ Arrays/largestNumber.java
@@ -0,0 +1,24 @@
+public class largestNumber {
+    public static int getlargest(int number[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for(int i=0;i<number.length; i++){
+            if (largest<number[i]) {
+                largest = number[i];
+            }
+            if (smallest>number[i]) {
+                smallest =  number[i];
+            }
+
+
+        }
+        System.out.println("Smallest value is :"+smallest);
+        return largest;
+    }
+    public static void main(String[] args) {
+        int number[]={1,2,6,3,5};
+
+        System.out.println("largest valu is:"+getlargest(number));
+    }
+}
