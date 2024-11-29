06840d6a0483821e4477a13b42a0955ba65313af
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-11-29 17:47:44 +0530

    Add files via upload

---
+++ String/string/Quse06.java
@@ -0,0 +1,29 @@
+package string;
+
+public class Quse06 {
+
+    public static String otUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String str = "hi, i am jayesh";
+        System.out.println(otUpperCase(str));
+    }
+}
