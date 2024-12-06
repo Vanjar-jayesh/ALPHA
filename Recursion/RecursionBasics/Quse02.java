5deae57245bd99088d54ca3e346925bd4a9fe9a5
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-12-06 11:45:02 +0530

    Add files via upload

---
+++ Recursion/RecursionBasics/Quse02.java
@@ -0,0 +1,17 @@
+package RecursionBasics;
+
+public class Quse02 {
+
+    public static void printInc(int n){
+        if (n==1) {
+            System.out.print(n+" ");
+            return;         
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printInc(n);
+    }
+}
