1e77499614779d43e57cd5732efec8eedf4a0188
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-12-06 11:45:45 +0530

    Add files via upload

---
+++ Recursion/RecursionBasics/Quse05.java
@@ -0,0 +1,21 @@
+package RecursionBasics;
+
+public class Quse05 {
+
+    public static int fib(int n ){
+        if (n==0||n==1) {
+            return n;
+        }
+
+        int f1 = fib(n-1);
+        int f2 =  fib(n-2);
+        int fn = f1+f2;
+        return fn;
+    }
+ public static void main(String[] args) {
+    
+    int n = 5;
+
+    System.out.println(fib(24));
+ }   
+}
