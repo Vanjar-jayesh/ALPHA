22f1ced1fadc138ee43a0edfd90579aea022a671
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-12-14 20:23:02 +0530

    Add files via upload

---
+++ ArrayList/baseQuse/Quse01.java
@@ -0,0 +1,22 @@
+package baseQuse;
+
+import java.util.ArrayList;
+
+public class Quse01 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(5);
+
+        for(int i = al.size()-1; i>=0; i--){
+            System.out.print(al.get(i)+" ");
+        }
+
+      
+    }
+}
