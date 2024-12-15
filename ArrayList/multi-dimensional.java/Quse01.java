2b06b3af322a1ce6db9332e8ab473f6c858c392d
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-12-15 13:42:12 +0530

    Add files via upload

---
+++ ArrayList/multi-dimensional.java/Quse01.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Quse01 {
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainla = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+
+        mainla.add(list);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(4);
+        mainla.add(list2);
+
+        // System.out.println(mainla);
+
+        for (int i = 0; i < mainla.size(); i++) {
+            ArrayList<Integer> currList = mainla.get(i);
+            for (int j = 0; j < currList.size(); j++) {
+                System.out.print(currList.get(j)+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(mainla);
+    }
+}
---
+++ ArrayList/multi-dimensional.java/Quse02.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Quse02 {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            list1.add(i * 1);// 12345
+            list2.add(i * 2);// 246810
+            list3.add(i * 3);// 361215
+
+        }
+
+        mainlist.add(list1);
+        mainlist.add(list2);
+        mainlist.add(list3);
+
+        // list2.remove(3);
+        // list2.remove(2);
+
+        System.out.println(mainlist);
+        // nested loops
+
+        for (int i = 0; i < mainlist.size(); i++) {
+            ArrayList<Integer> currlist = mainlist.get(i);
+            for (int j = 0; j < currlist.size(); j++) {
+                System.out.print(currlist.get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
