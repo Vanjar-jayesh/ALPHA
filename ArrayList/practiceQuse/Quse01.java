5dc639568c02457f9eef833c4f1785b9322c9d25
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-12-16 14:04:19 +0530

    Add files via upload

---
+++ ArrayList/practiceQuse/Quse01.java
@@ -0,0 +1,35 @@
+package practiceQuse;
+
+import java.util.ArrayList;
+
+public class Quse01 {
+
+    public static boolean monotonic(ArrayList<Integer> nums){
+
+        boolean incr = true;
+        boolean dec = true;
+
+        for(int i=0; i<nums.size()-1; i++){
+
+            if (nums.get(i)< nums.get(i+1)) {
+                incr =false;
+            }
+
+            if (nums.get(i) > nums.get(i+1)) {
+                dec =  false;
+            }
+        }
+
+        return incr || dec;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        nums.add(1);
+        nums.add(3);
+        nums.add(2);
+        // nums.add(3);
+
+        System.out.println(monotonic(nums));
+    }
+}
