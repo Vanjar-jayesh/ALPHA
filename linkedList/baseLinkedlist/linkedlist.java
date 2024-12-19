c225b2fd2b1f64ec3c6e81f6d99ca476d3e20e56
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-12-19 16:38:24 +0530

    Update linkedlist.java

--- linkedList/baseLinkedlist/linkedlist.java
+++ linkedList/baseLinkedlist/linkedlist.java
@@ -305,9 +305,69 @@ public class linkedlist {
         prev.next = null;
     }
 
-
-
     
+    // merge Sort on a linked list
+
+    public Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;// +1
+            fast = fast.next.next;// +2
+        }
+        return slow;// slow is my midNOde
+    }
+
+    private Node merge(Node head1,Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return mergedLL.next;
+        
+    }
+    public Node mergesort(Node head){
+        if (head==null || head.next == null) {
+            return head;
+        }
+        // find min
+        Node mid = getMid(head);
+        // lest & right Ms
+        Node rightHead = mid.next;
+        mid.next = null;
+       Node newLeft =  mergesort(head);
+       Node newRight =  mergesort(rightHead);
+
+
+        // merge
+
+        return merge(newLeft,newRight);
+    }
     public static void main(String[] args) {
         linkedlist ll = new linkedlist();
         ll.printlist();
