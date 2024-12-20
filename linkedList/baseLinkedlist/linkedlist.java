7c30a7aa63770230ae064ef93b94bce14bf0a72e
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-12-20 20:08:20 +0530

    Update linkedlist.java

--- linkedList/baseLinkedlist/linkedlist.java
+++ linkedList/baseLinkedlist/linkedlist.java
@@ -408,6 +408,45 @@ public class linkedlist {
             right = NextR;
         }
     }
+     public void swapNode(int x , int y){
+        if (x == y) {
+            return;
+        }
+
+        Node prevX = null,currX = head;
+
+        while (currX != null && currX.data != x) {
+            prevX = currX;
+            currX = currX.next;
+
+        }
+
+        Node prevY = null, currY= head;
+        while (currY != null && currY.data != y) {
+            prevY = currY;
+            currY = currY.next;
+        }
+
+        if (currX == null || currY == null) {
+            return;
+        }
+        if (prevX != null) {
+            prevX.next = currY;
+        }else{
+            head = currY;
+        }
+
+        if (prevY != null) {
+            prevY.next = currX;
+        }else{
+            head = currX;
+        }
+
+        Node temp = currX.next;
+        currX.next = currY.next;
+        currY.next  = temp;
+
+    }
     public static void main(String[] args) {
         linkedlist ll = new linkedlist();
 
