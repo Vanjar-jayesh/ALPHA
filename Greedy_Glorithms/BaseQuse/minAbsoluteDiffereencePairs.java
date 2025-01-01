b1ebcbb243312a712a20e8bf00ca8b91f1d7ebab
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2025-01-01 15:02:36 +0530

    Add files via upload

---
+++ Greedy_Glorithms/BaseQuse/minAbsoluteDiffereencePairs.java
@@ -0,0 +1,22 @@
+package BaseQuse;
+
+import java.util.Arrays;
+
+public class minAbsoluteDiffereencePairs {
+    public static void main(String[] args) {
+        int A[] = {1,7,5};
+        int B[] = {2,3,5};
+
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+
+        for(int i=0; i<A.length;i++){
+            minDiff += Math.abs(A[i]-B[i]);
+        }
+
+        System.out.println("MinDiif value = : "+minDiff);
+    }
+}
