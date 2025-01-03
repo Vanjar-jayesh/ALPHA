43331fb0b4ff53629ab7750ad5ada1c8910beb7a
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2025-01-03 21:16:43 +0530

    Add files via upload

---
+++ BinaryTreesB/BaseQuse/SumofNode.java
@@ -0,0 +1,49 @@
+package BaseQuse;
+
+public class SumofNode {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+
+    public static int sumNode(Node root){
+        if (root == null) {
+            return 0;
+
+        }
+
+        int lsum = sumNode(root.left);
+        int rsum = sumNode(root.right);
+
+        return lsum + rsum + root.data;
+    }
+
+    public static void main(String[] args) {
+        /*
+         *          1
+         *         / \
+         *        2   3
+         *       / \ / \
+         *      4  5 6  7
+         */
+
+         Node root = new Node(1);
+         root.left = new Node(2);
+         root.right = new Node(3);
+         root.left.left = new Node(4);
+         root.left.right = new Node(5);
+         root.right.left = new Node(6);
+         root.right.right = new Node(7);
+
+         System.out.println(sumNode(root));
+    }
+}
